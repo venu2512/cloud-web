3de96c0b74aac1aec46e659c815ec5fc060b168e
venu2512 <venu25122005@gmail.com>
2026-03-06 20:11:02 +0530

    fix mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/cloudDB");
 
-    console.log("MongoDB Connected Successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
   } catch (error) {
+
     console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
+
   }
 };
 
