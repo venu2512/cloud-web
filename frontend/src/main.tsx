09bd366eedd0689a75cca8df9ebb168fa482adcc
venu2512 <venu25122005@gmail.com>
2026-03-11 15:05:26 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Keep Render backend alive
+const ping = () => fetch("https://cloud-nova.onrender.com/").catch(() => {});
+ping();
+setInterval(ping, 10 * 60 * 1000);
+
 createRoot(document.getElementById("root")!).render(<App />);
